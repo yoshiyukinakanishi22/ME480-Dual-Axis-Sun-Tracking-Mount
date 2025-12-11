d809429af2292d102fdf1e9966a554c8cbe15800
Yoshiyuki Nakanishi <yoshin22@uw.edu>
2025-12-10 17:47:40 -0800

    final code

--- sun_tracking/sun_tracking.ino
+++ sun_tracking/sun_tracking.ino
@@ -8,20 +8,24 @@
 // FR Forward Right
 // AR Aft Right
 // Coordinate System defined from the middle of the hinge 
-#define ldrFLPin  34
-#define ldrALPin  35
-#define ldrFRPin  32
-#define ldrARPin  33
-#define ldrLaUPPin   26
-#define ldrLaDOWNPin 27
-// 35, 34 top two LDRs
+#define ldrFLPin  27
+#define ldrALPin  32
+#define ldrFRPin  35
+#define ldrARPin  26
+#define ldrLaUPPin   25
+#define ldrLaDOWNPin 33
+//12 (Right)  27, 26   
+//34 (Top)   25, 33    
+//56 (Left)  32, 35    
+
+// 35, 25 top two LDRs
 // 32, 33 middle two LDRs
 // 26, 27 bottom two LDRssx
-#define rotStepPin 14
-#define rotDirPin 13
+#define rotStepPin 13
+#define rotDirPin 14
 #define linactENA 15
-#define linactIN1 2
-#define linactIN2 4
+#define linactIN1 4
+#define linactIN2 2
 
 
 
@@ -35,7 +39,7 @@ int rpm = 400;
 
 Stepper myStepper (100, rotStepPin, rotDirPin); //set STEPS to 100 if error
 
-int size = 5;
+int size = 3; //change to 10?
 int readIndex = 0;
 int readingsFL[5];
 int readingsAL[5];
@@ -127,7 +131,7 @@ void loop() {
   totalLaDOWN = totalLaDOWN + readingsLaDOWN[readIndex];
   averageLaDOWN = totalLaDOWN / float(size);
   
-  delay(500);
+  delay(10);
   
   readIndex = (readIndex + 1) % size;
   int diffLeft = averageFL - averageAL;
@@ -135,49 +139,47 @@ void loop() {
   int dod = diffLeft - diffRight;
   int diffLA = averageLaUP - averageLaDOWN;
 
-  // if (dod >= 300){
-  //    // When dod is positive, that means left side is getting more sunlight
-  //     Serial.print("Out of Range DOD Pos");	    
-  //     Serial.println(dod);
-  //     Serial.print("34: "); Serial.println(averageFL);
-  //     Serial.print("35: "); Serial.println(averageAL);
-  //     Serial.print("32: "); Serial.println(averageFR);
-  //     Serial.print("33: "); Serial.println(averageAR);
-  //     myStepper.step(50);
-  //   }
-  // else if (dod <= -700){
-  //     Serial.print("Out of Range DOD Neg");	    
-  //     Serial.println(dod);
-  //     Serial.print("34: "); Serial.println(averageFL);
-  //     Serial.print("35: "); Serial.println(averageAL);
-  //     Serial.print("32: "); Serial.println(averageFR);
-  //     Serial.print("33: "); Serial.println(averageAR);
-  //   	myStepper.step(-50);
-  // }
-  // else{
-  //   Serial.println("Within Range");
-  //   Serial.print("34: "); Serial.println(averageFL);
-  //   Serial.print("35: "); Serial.println(averageAL);
-  //   Serial.print("32: "); Serial.println(averageFR);
-  //   Serial.print("33: "); Serial.println(averageAR);
+  if (dod >= 300){
+     // When dod is positive, that means left side is getting more sunlight
+      Serial.print("Out of Range DOD Pos");	    
+      Serial.println(dod);
+      Serial.print("34: "); Serial.println(averageFL);
+      Serial.print("35: "); Serial.println(averageAL);
+      Serial.print("32: "); Serial.println(averageFR);
+      Serial.print("33: "); Serial.println(averageAR);
+      myStepper.step(1);
+    }
+  else if (dod <= -700){
+      Serial.print("Out of Range DOD Neg");	    
+      Serial.println(dod);
+      Serial.print("34: "); Serial.println(averageFL);
+      Serial.print("35: "); Serial.println(averageAL);
+      Serial.print("32: "); Serial.println(averageFR);
+      Serial.print("33: "); Serial.println(averageAR);
+    	myStepper.step(-1);
+  }
+  else{
+    Serial.println("Within Range");
+    Serial.print("34: "); Serial.println(averageFL);
+    Serial.print("35: "); Serial.println(averageAL);
+    Serial.print("32: "); Serial.println(averageFR);
+    Serial.print("33: "); Serial.println(averageAR);
     
-  // }
+  }
 
   if (diffLA >= 400){
     // Extend LA
     Serial.println("Retracting");
     Serial.println(averageLaUP);
     Serial.println(averageLaDOWN);
-    digitalWrite(linactIN1, LOW);
-    digitalWrite(linactIN2, HIGH);
+    goBackward();
   }
   else if (diffLA <= -400){
     // Retracts LA
     Serial.println("Extending");
     Serial.println(averageLaUP);
     Serial.println(averageLaDOWN);
-    digitalWrite(linactIN1, HIGH);
-    digitalWrite(linactIN2, LOW);
+    goForward();
   }
   else{
     // Stop Actuator
@@ -186,10 +188,24 @@ void loop() {
     Serial.println("Stay Same");
     Serial.println(averageLaUP);
     Serial.println(averageLaDOWN);
-    digitalWrite(linactIN1, HIGH);
-    digitalWrite(linactIN2, HIGH);
+    stay();
   }
   }
 
+
+void goForward(){
+  digitalWrite(linactIN1, LOW);
+  digitalWrite(linactIN2, HIGH);
+}
+
+void goBackward(){
+  digitalWrite(linactIN1, HIGH);
+  digitalWrite(linactIN2, LOW);
+}
+
+void stay(){
+  digitalWrite(linactIN1, HIGH);
+  digitalWrite(linactIN2, HIGH);  
+}
 
 
